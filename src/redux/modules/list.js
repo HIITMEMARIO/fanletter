4e02482d300295570a62494da842bc0907d357b4
HIITMEMARIO <alswnsgur@nate.com>
2023-11-20 08:52:56 +0900

    read me 추가

--- src/redux/modules/list.js
+++ src/redux/modules/list.js
@@ -28,7 +28,7 @@ const initialState = {
         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/646.jpg',
       content:
         '윈터1 Itaque nihil quae neque itaque. Non a officiis ducimus nemo consectetur. Ducimus libero voluptatum consequuntur.',
-      writedTo: '혜린',
+      writedTo: '해린',
       id: '3',
     },
     {
@@ -38,7 +38,7 @@ const initialState = {
         'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/37.jpg',
       content:
         '닝닝1 Sint qui eligendi repudiandae placeat maiores repudiandae assumenda repudiandae. Distinctio commodi iste. Qui architecto iusto.',
-      writedTo: '해인',
+      writedTo: '혜인',
       id: '4',
     },
     {
